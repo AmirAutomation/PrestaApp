954ea1d6a931810e9d3a483844d0b4d5378c0fe5
Amir <ingenieroamir@gmail.com>
2023-04-16 19:43:52 -0500

    update

--- src/test/java/co/com/project/automation/prestaApp/runners/Run.java
+++ src/test/java/co/com/project/automation/prestaApp/runners/Run.java
@@ -3,19 +3,17 @@ package co.com.project.automation.prestaApp.runners;
 import co.com.project.automation.prestaApp.util.BeforeSuite;
 import co.com.project.automation.prestaApp.util.DataToFeature;
 import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.junit.runner.RunWith;
 
 import java.io.IOException;
 
-
+@RunWith(RunnerPersonalizado.class)
 @CucumberOptions(features = "src/test/resources/features/PrestaApp.feature",
         glue = "co.com.project.automation.prestaApp.stepdefinitions",
         tags = "@T001",
         snippets = CucumberOptions.SnippetType.CAMELCASE)
-@RunWith(RunnerPersonalizado.class)
+
 public class Run {
     @BeforeSuite
     public static void test() throws InvalidFormatException, IOException {
